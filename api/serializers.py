be8d2655c7dd3b610512fc2f7dbf733b0aa4266a
mauriciocordeiro <mauriciocordeiro@live.com>
2019-12-04 19:25:52 -0300

    serializing relations

--- api/serializers.py
+++ api/serializers.py
@@ -3,17 +3,20 @@ from .models import Pessoa
 from .models import Endereco
 from .models import Telefone
 
-class PessoaSerializer(serializers.HyperlinkedModelSerializer):
+class PessoaSerializer(serializers.ModelSerializer):
+    telefones = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+    enderecos = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+
     class Meta:
         model = Pessoa
-        fields = ('id', 'nome', 'sobrenome', 'nascimento', 'email', 'foto')
+        fields = ('id', 'nome', 'sobrenome', 'nascimento', 'email', 'foto', 'telefones', 'enderecos')
         
-class EnderecoSerializer(serializers.HyperlinkedModelSerializer):
+class EnderecoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Endereco
         fields = ('id', 'id_pessoa', 'logradouro', 'numero', 'complemento', 'bairro', 'cep', 'cidade', 'uf', 'tipo')
         
-class TelefoneSerializer(serializers.HyperlinkedModelSerializer):
+class TelefoneSerializer(serializers.ModelSerializer):
     class Meta:
         model = Telefone
         fields = ('id', 'id_pessoa', 'numero', 'tipo')
