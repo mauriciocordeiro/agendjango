af22bbfdb628404a364f548762f17c9bcb204363
Maurício Cordeiro <mauriciocordeiro@live.com>
2019-12-05 10:06:27 -0300

    Update settings.py

--- agendjango/settings.py
+++ agendjango/settings.py
@@ -37,7 +37,17 @@ CORS_ALLOW_METHODS = [
     'POST',
     'PUT',
 ]
-
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
 
 
 # Application definition
